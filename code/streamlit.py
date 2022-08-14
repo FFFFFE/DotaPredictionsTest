c7087d48b9323f69e87958dfe66f8319f855dd39
Shapura <ter-shapura@mail.ru>
2022-08-14 16:07:14 +0700

    фикс обработчика ошибки при выборе неверного колличества героев в пике

--- code/streamlit.py
+++ code/streamlit.py
@@ -55,12 +55,15 @@ with st.container():
         dire_pick = st.multiselect("Выберите 5 героев команды Dire: ", heroes_list,
                                    ['Io', 'Razor', 'Zeus', 'Lycan', 'Doom'])
 
-    if len(rad_pick) == len(dire_pick) == 5:
-        is_rad_win = make_predict.make_predict(rad_pick, dire_pick, rad_team, dire_team)
-        winner = [f'Победит команда {dire_team}', f'Победит команда {rad_team}'][is_rad_win]
+    # if len(rad_pick) == len(dire_pick) == 5:
+    #     is_rad_win = make_predict.make_predict(rad_pick, dire_pick, rad_team, dire_team)
+    #     winner = [f'Победит команда {dire_team}', f'Победит команда {rad_team}'][is_rad_win]
 
     if(st.button("Сделать предсказание")):
         if len(rad_pick) == len(dire_pick) == 5:
+            is_rad_win = make_predict.make_predict(rad_pick, dire_pick, rad_team, dire_team)
+            winner = [f'Победит команда {dire_team}', f'Победит команда {rad_team}'][is_rad_win]
+
             st.success(winner)
         else:
             st.error('В каждой команде должно быть выбрано 5 героев')
