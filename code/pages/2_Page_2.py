89f59967ab96a7f45f857cf851849427ec43a080
Shapura <ter-shapura@mail.ru>
2022-09-05 19:39:40 +0700

    попытка мультипейджа

--- code/pages/2_Page_2.py
+++ code/pages/2_Page_2.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.insert(1, "code/streamlit.py") # Edit <p2_functions folder path> and put path
 
-from func_2 import multiply_two
+from streamlit import streamlit as st
 
 st.wirte('Приветик :)')
