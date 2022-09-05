90a37acacd52249727dbafbdc4c93c6e0acc048f
Shapura <ter-shapura@mail.ru>
2022-09-05 19:41:37 +0700

    попытка мультипейджа

--- code/pages/2_Page_2.py
+++ code/pages/2_Page_2.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(1, "code/streamlit.py") # Edit <p2_functions folder path> and put path
+sys.path.insert(1, "code/") # Edit <p2_functions folder path> and put path
 
 from streamlit import streamlit as st
 
