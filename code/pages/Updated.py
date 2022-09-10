621872a321da147dd2a552892a78229a0bc95cac
Shapura <ter-shapura@mail.ru>
2022-09-10 22:38:15 +0700

    тест мультипейджа

--- code/pages/Updated.py
+++ code/pages/Updated.py
@@ -12,13 +12,6 @@ with open(os.path.abspath("data/teamid_stats.txt"), 'r', encoding='utf-8') as fi
     teamid_stats = eval(file.read())
 
 teams_list = list(teams_dict.keys())
-tundra_list = teams_list.copy()
-
-
-teams_list.remove('OG')
-tundra_list.remove('Tundra Esports')
-teams_list = ['OG'] + teams_list
-tundra_list = ['Tundra Esports'] + tundra_list
 
 with st.container():
     st.write("---")
@@ -27,7 +20,7 @@ with st.container():
         rad_team = st.selectbox("Команда Radiant: ", teams_list)
 
     with right_column:
-        dire_team = st.selectbox("Команда Dire: ", tundra_list)
+        dire_team = st.selectbox("Команда Dire: ", teams_list)
 
     if (st.button("Сделать предсказание")):
         st.write('Ещё не 17:19')
