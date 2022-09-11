2b416c79f6ad01fd74d7f2017dd3e0718c23ed49
Shapura <ter-shapura@mail.ru>
2022-09-11 12:05:52 +0700

    движусь к новой модели

--- code/pages/Updated.py
+++ code/pages/Updated.py
@@ -1,12 +1,26 @@
 import streamlit as st
+import sys
 import os
-import make_predict_upd
 
 with open(os.path.abspath("data/teams_dict.txt"), 'r', encoding='utf-8') as file:
     teams_dict = eval(file.read())
 
 with open(os.path.abspath("data/teamid_stats.txt"), 'r', encoding='utf-8') as file:
     teamid_stats = eval(file.read())
+
+from_file = CatBoostClassifier()
+clf = from_file.load_model(os.path.abspath("data/model_eval.cbm"))
+
+def make_predict_upd(rad_team, dire_team):
+    rad_team_id, dire_team_id = teams_dict[rad_team], teams_dict[dire_team]
+
+    new_match = [teamid_stats[rad_team_id][2]] + [teamid_stats[dire_team_id][2]]
+
+    # 1 - radiant_win, 0 - dire_win
+    predict = clf.predict(new_match)
+    probability = clf.predict(new_match, prediction_type='Probability')[0]
+    return predict, probability
+
 
 teams_list = list(teams_dict.keys())
 
@@ -20,7 +34,7 @@ with st.container():
         dire_team = st.selectbox("Команда Dire: ", teams_list)
 
     if (st.button("Сделать предсказание")):
-        st.write(make_predict_upd.make_predict_upd(rad_team, dire_team))
+        st.write(str(make_predict_upd(rad_team, dire_team)))
 
         # is_rad_win, probability = make_predict_upd.make_predict_upd(rad_team, dire_team)
         # probability = round(max(probability) * 100)
