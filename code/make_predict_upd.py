889c3ff2c89a79e8d7853fed8de01ee35fc65963
Shapura <ter-shapura@mail.ru>
2022-09-11 11:51:11 +0700

    движусь к новой модели

--- code/make_predict_upd.py
+++ code/make_predict_upd.py
@@ -17,11 +17,11 @@ with open(os.path.abspath("data/teamid_stats.txt"), 'r', encoding='utf-8') as fi
 
 
 def make_predict_upd(rad_team, dire_team):
-     rad_team_id, dire_team_id = teams_dict[rad_team], teams_dict[dire_team]
-     new_match = [teamid_stats[rad_team_id][2]] + [teamid_stats[dire_team_id][2]]
+    rad_team_id, dire_team_id = teams_dict[rad_team], teams_dict[dire_team]
+    new_match = [teamid_stats[rad_team_id][2]] + [teamid_stats[dire_team_id][2]]
 
-     # 1 - radiant_win, 0 - dire_win
-     predict = clf.predict(new_match)
-     probability = clf.predict(new_match, prediction_type='Probability')[0]
+    # 1 - radiant_win, 0 - dire_win
+    predict = clf.predict(new_match)
+    probability = clf.predict(new_match, prediction_type='Probability')[0]
 
-     return predict, probability
+    return predict, probability
