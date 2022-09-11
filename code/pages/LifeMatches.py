ebe1391e2720c91500f8d4ff76c5ff8ae715033b
Shapura <ter-shapura@mail.ru>
2022-09-12 00:23:09 +0700

    онлайн матчи

--- code/pages/LifeMatches.py
+++ code/pages/LifeMatches.py
@@ -22,10 +22,10 @@ life_df = pd.json_normalize(live_games['result']['games'])
 
 life_df = life_df[['match_id', 'radiant_team.team_name', 'radiant_team.team_id', 'dire_team.team_id'
                     , 'dire_team.team_name']]
-life_df = life_df[['radiant_team.team_id', 'dire_team.team_id']].apply(int)
+life_df = life_df['radiant_team.team_id'].apply(int)
 
 st.dataframe(life_df)
 
-cool_teams = [int(t) for t in life_df['dire_team.team_id'].tolist() if t in teams_list]
+cool_teams = [t for t in life_df['dire_team.team_id'].tolist() if t in teams_list]
 
 st.dataframe(life_df[life_df['dire_team.team_id'].isin(cool_teams)])
