8f045314f8118822f44c7d38adabce576f366101
Shapura <ter-shapura@mail.ru>
2022-09-12 01:00:49 +0700

    онлайн матчи

--- code/pages/LifeMatches.py
+++ code/pages/LifeMatches.py
@@ -34,16 +34,17 @@ live_games = json.loads(r_steam.text)
 
 life_df = pd.json_normalize(live_games['result']['games'])
 life_df = life_df[['match_id', 'radiant_team.team_name', 'radiant_team.team_id', 'dire_team.team_name', 'dire_team.team_id']]
-life_df.rename(columns={'radiant_team.team_name': 'radiant_team', 'dire_team.team_name': 'dire_team',
+life_df = life_df.rename(columns={'radiant_team.team_name': 'radiant_team', 'dire_team.team_name': 'dire_team',
                         'radiant_team.team_id': 'rad_team_id', 'dire_team.team_id': 'dire_team_id'})
 life_df.dropna(inplace=True)
 
-life_df[['radiant_team.team_id', 'dire_team.team_id']] = life_df[['radiant_team.team_id', 'dire_team.team_id']].astype('int64')
+life_df[['rad_team_id', 'dire_team_id']] = life_df[['rad_team_id', 'dire_team_id']].astype('int64')
 life_df.reset_index(drop=True, inplace=True)
 
 
-cool_teams_1 = [t for t in life_df['dire_team.team_id'].tolist() if t in teams_id_list]
-cool_teams_2 = [t for t in life_df['dire_team.team_id'].tolist() if t in teams_id_list]
+cool_teams_1 = [t for t in life_df['rad_team_id'].tolist() if t in teams_id_list]
+cool_teams_2 = [t for t in life_df['dire_team_id'].tolist() if t in teams_id_list]
 cool_teams = set(cool_teams_1).union(set(cool_teams_2))
 
-st.dataframe(life_df[life_df['dire_team.team_id'].isin(cool_teams)][['match_id', 'radiant_team.team_name', 'dire_team.team_name']])
+st.dataframe(life_df[(life_df['rad_team_id'].isin(cool_teams)) &
+             (life_df['dire_team_id'].isin(cool_teams))][['match_id', 'radiant_team', 'dire_team']])
