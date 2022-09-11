6b9aceb526d183d4d243907cb244f094fc506f6b
Shapura <ter-shapura@mail.ru>
2022-09-12 00:30:09 +0700

    онлайн матчи

--- code/pages/LifeMatches.py
+++ code/pages/LifeMatches.py
@@ -21,7 +21,8 @@ live_games = json.loads(r_steam.text)
 life_df = pd.json_normalize(live_games['result']['games'])
 life_df = life_df[['match_id', 'radiant_team.team_name', 'radiant_team.team_id', 'dire_team.team_name', 'dire_team.team_id']]
 life_df.dropna(inplace=True)
-life_df[['radiant_team.team_id', 'dire_team.team_name']] = life_df[['radiant_team.team_id', 'dire_team.team_name']].astype('int64')
+
+life_df[['radiant_team.team_id', 'dire_team.team_id']] = life_df[['radiant_team.team_id', 'dire_team.team_id']].astype('int64')
 
 st.dataframe(life_df[['match_id', 'radiant_team.team_name', 'dire_team.team_name']])
 
