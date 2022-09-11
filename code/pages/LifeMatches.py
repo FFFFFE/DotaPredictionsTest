671ca08139be277a6773568c793559675b0de31e
Shapura <ter-shapura@mail.ru>
2022-09-12 00:29:13 +0700

    онлайн матчи

--- code/pages/LifeMatches.py
+++ code/pages/LifeMatches.py
@@ -19,13 +19,11 @@ r_steam = requests.get(f'https://api.steampowered.com/IDOTA2Match_570/GetLiveLea
 live_games = json.loads(r_steam.text)
 
 life_df = pd.json_normalize(live_games['result']['games'])
-
-life_df = life_df[['match_id', 'radiant_team.team_name', 'radiant_team.team_id', 'dire_team.team_id'
-                    , 'dire_team.team_name']]
+life_df = life_df[['match_id', 'radiant_team.team_name', 'radiant_team.team_id', 'dire_team.team_name', 'dire_team.team_id']]
 life_df.dropna(inplace=True)
-life_df['radiant_team.team_id'] = life_df['radiant_team.team_id'].astype('int64')
+life_df[['radiant_team.team_id', 'dire_team.team_name']] = life_df[['radiant_team.team_id', 'dire_team.team_name']].astype('int64')
 
-st.dataframe(life_df)
+st.dataframe(life_df[['match_id', 'radiant_team.team_name', 'dire_team.team_name']])
 
 cool_teams = [t for t in life_df['dire_team.team_id'].tolist() if t in teams_list]
 
