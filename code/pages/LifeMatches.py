4bad8b4bd0b5ba5cf041720a7b2ac84747c90893
Shapura <ter-shapura@mail.ru>
2022-09-12 00:06:41 +0700

    онлайн матчи

---
+++ code/pages/LifeMatches.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import requests
+import json
+import os
+
+key = '3A65F973BAF1C8130DCD77B739C74EC9'
+
+with open(os.path.abspath("data/teams_dict.txt"), 'r', encoding='utf-8') as file:
+    teams_dict = eval(file.read())
+
+with open(os.path.abspath("data/teamid_stats.txt"), 'r', encoding='utf-8') as file:
+    teamid_stats = eval(file.read())
+
+teams_list = list(teams_dict.keys())
+
+r_steam = requests.get(f'https://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v1/?key={key}')
+live_games = json.loads(r_steam.text)
+
+life_df = pd.json_normalize(live_games['result']['games'])
+
+life_df = life_df[['match_id', 'radiant_team.team_name', 'radiant_team.team_id', 'dire_team.team_id'
+                    , 'dire_team.team_name']]
+
+st.dataframe(life_df)
