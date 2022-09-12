97a8433828bd745b6a4e20dc84607102c179b006
Shapura <ter-shapura@mail.ru>
2022-09-12 12:22:02 +0700

    онлайн матчи

--- code/pages/LifeMatches.py
+++ code/pages/LifeMatches.py
@@ -34,35 +34,34 @@ with open(os.path.abspath("data/teamid_stats.txt"), 'r', encoding='utf-8') as fi
 teams_id_list = list(teamid_stats.keys())
 
 r_steam = requests.get(f'https://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v1/?key={steam_key}')
-st.write(r_steam.status_code)
-live_games = json.loads(r_steam.text)
+if r_steam.status_code != 200:
+    st.error(f'Ошибка {r_steam.status_code}. Попробуйте перезагрузить страницу')
+else:
+    live_games = json.loads(r_steam.text)
+    life_df = pd.json_normalize(live_games['result']['games'])
+    important_cols = ['match_id', 'radiant_team.team_name', 'radiant_team.team_id', 'dire_team.team_name',
+                      'dire_team.team_id']
+
+    if set(important_cols) - set(life_df.columns) != 0:
+        st.error('Перезагрузите страницу')
+    else:
+        life_df = life_df[important_cols]
+        life_df = life_df.rename(columns={'radiant_team.team_name': 'radiant_team', 'dire_team.team_name': 'dire_team',
+                                'radiant_team.team_id': 'rad_team_id', 'dire_team.team_id': 'dire_team_id'})
+        life_df.dropna(inplace=True)
+        life_df[['rad_team_id', 'dire_team_id']] = life_df[['rad_team_id', 'dire_team_id']].astype('int64')
+
+        filtered_df = life_df[(life_df['rad_team_id'].isin(teams_id_list)) & (life_df['dire_team_id'].isin(teams_id_list))]
+        filtered_df.reset_index(drop=True, inplace=True)
+
+        filtered_df[['winner_side', 'probability']] = filtered_df.apply(lambda x: make_predict_upd(x['radiant_team'],
+                                                                                              x['dire_team']), axis=1).tolist()
+        filtered_df['winner_side'] = filtered_df['winner_side'].apply(lambda x: ["dire_team", "radiant_team"][x])
+        filtered_df['probability'] = filtered_df['probability'].apply(lambda x: round(max(x), 4))
 
 
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
+        filtered_df['winner_predict'] = filtered_df.apply(lambda x: ([x['radiant_team'], x['dire_team']]
+                                                                [x['winner_side'] == 'dire_team']), axis=1).tolist()
 
-
-life_df = pd.json_normalize(live_games['result']['games'])
-life_df = life_df[['match_id', 'radiant_team.team_name', 'radiant_team.team_id', 'dire_team.team_name', 'dire_team.team_id']]
-life_df = life_df.rename(columns={'radiant_team.team_name': 'radiant_team', 'dire_team.team_name': 'dire_team',
-                        'radiant_team.team_id': 'rad_team_id', 'dire_team.team_id': 'dire_team_id'})
-life_df.dropna(inplace=True)
-life_df[['rad_team_id', 'dire_team_id']] = life_df[['rad_team_id', 'dire_team_id']].astype('int64')
-
-filtered_df = life_df[(life_df['rad_team_id'].isin(teams_id_list)) & (life_df['dire_team_id'].isin(teams_id_list))]
-filtered_df.reset_index(drop=True, inplace=True)
-
-filtered_df[['winner_side', 'probability']] = filtered_df.apply(lambda x: make_predict_upd(x['radiant_team'],
-                                                                                      x['dire_team']), axis=1).tolist()
-filtered_df['winner_side'] = filtered_df['winner_side'].apply(lambda x: ["dire_team", "radiant_team"][x])
-filtered_df['probability'] = filtered_df['probability'].apply(lambda x: round(max(x), 4))
-
-
-filtered_df['winner_predict'] = filtered_df.apply(lambda x: ([x['radiant_team'], x['dire_team']]
-                                                        [x['winner_side'] == 'dire_team']), axis=1).tolist()
-
-st.markdown('## Матчи, идущие в настоящий момент')
-st.dataframe(filtered_df[['match_id', 'radiant_team', 'dire_team', 'winner_predict', 'probability']])
+        st.markdown('## Матчи, идущие в настоящий момент')
+        st.dataframe(filtered_df[['match_id', 'radiant_team', 'dire_team', 'winner_predict', 'probability']])
