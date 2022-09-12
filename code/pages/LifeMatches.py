28ac64b125bcc57befe5dcdfb73e58107ec6b712
Shapura <ter-shapura@mail.ru>
2022-09-12 12:11:16 +0700

    онлайн матчи

--- code/pages/LifeMatches.py
+++ code/pages/LifeMatches.py
@@ -20,10 +20,6 @@ def make_predict_upd(rad_team, dire_team):
     return predict, probability
 
 
-def find_winner(rad_team, dire_team, winner_predict):
-    return rad_team if winner_predict == 'radiant_team' else dire_team
-
-
 from_file = CatBoostClassifier()
 clf = from_file.load_model(os.path.abspath("data/model_eval.cbm"))
 
@@ -50,16 +46,14 @@ life_df[['rad_team_id', 'dire_team_id']] = life_df[['rad_team_id', 'dire_team_id
 filtered_df = life_df[(life_df['rad_team_id'].isin(teams_id_list)) & (life_df['dire_team_id'].isin(teams_id_list))]
 filtered_df.reset_index(drop=True, inplace=True)
 
-filtered_df[['winner_predict', 'probability']] = filtered_df.apply(lambda x: make_predict_upd(x['radiant_team'],
+filtered_df[['winner_side', 'probability']] = filtered_df.apply(lambda x: make_predict_upd(x['radiant_team'],
                                                                                       x['dire_team']), axis=1).tolist()
-filtered_df['winner_predict'] = filtered_df['winner_predict'].apply(lambda x: ["dire_team", "radiant_team"][x])
+filtered_df['winner_side'] = filtered_df['winner_side'].apply(lambda x: ["dire_team", "radiant_team"][x])
 filtered_df['probability'] = filtered_df['probability'].apply(lambda x: round(max(x), 4))
 
-# filtered_df['test_column'] = filtered_df.apply(lambda x: find_winner(x['radiant_team'], x['dire_team'],
-#                                                                           x['winner_predict']), axis=1).tolist()
 
-filtered_df['test_column'] = filtered_df.apply(lambda x: ([x['radiant_team'], x['dire_team']]
-                                                        [x['winner_predict'] == 'dire_team']), axis=1).tolist()
+filtered_df['winner_predict'] = filtered_df.apply(lambda x: ([x['radiant_team'], x['dire_team']]
+                                                        [x['winner_side'] == 'dire_team']), axis=1).tolist()
 
 st.markdown('## Матчи, идущие в настоящий момент')
-st.dataframe(filtered_df[['match_id', 'radiant_team', 'dire_team', 'winner_predict', 'probability', 'test_column']])
+st.dataframe(filtered_df[['match_id', 'radiant_team', 'dire_team', 'winner_predict', 'probability']])
